815bc788de75747652928a0b7adcff237af2d60f
MaxGrigorev@github.com <MaxGrigorev@github.com>
2018-08-08 03:22:16 +0500

    style

--- client/src/Components/DogList.js
+++ client/src/Components/DogList.js
@@ -10,6 +10,7 @@ import {
 import { connect } from 'react-redux';
 
 import { listDogs } from '../Reducers/dogReducer';
+import * as Url from '../Constants/url';
 
 class DogList extends Component {
 
@@ -62,16 +63,15 @@ class DogList extends Component {
           title: item.breed,
           passProps: {
             name: item.breed,
-            img: item.img
+            img: Url.BASE_URL+'/uploads/'+item.img
           }
         })
       }
     >
-	  <View >
-	  	<Image
-	  source={{uri: 'http://192.168.1.7:8082/uploads/'+item.img}} style={{width: 400, height: 400}}/>
-	  	<Text style={styles.item}>{item.breed}</Text>
-	  </View>
+      <View >
+        <Text style={{fontSize:80}}>{item.breed}</Text>
+        <Image source={{uri: Url.BASE_URL+'/uploads/'+item.img}} style={{width: 400, height: 400}}/>
+      </View>
     </TouchableOpacity>
   );
   render() {
@@ -86,7 +86,7 @@ class DogList extends Component {
     //		{"_id":"5b4edca449d74f30e2b46567","breed":"kelpie","img":"https://images.dog.ceo/breeds/kelpie/n02105412_3078.jpg"},{"_id":"5b4edccd49d74f30e2b46568","breed":"rottweiler","img":"https://images.dog.ceo/breeds/rottweiler/n02106550_2832.jpg"},{"_id":"5b4edcf449d74f30e2b46569","breed":"beagle","img":"https://images.dog.ceo/breeds/beagle/n02088364_9650.jpg"},
     //		{"_id":"5b4edca449d74f30e2b46567","breed":"kelpie","img":"https://images.dog.ceo/breeds/kelpie/n02105412_3078.jpg"},{"_id":"5b4edccd49d74f30e2b46568","breed":"rottweiler","img":"https://images.dog.ceo/breeds/rottweiler/n02106550_2832.jpg"},{"_id":"5b4edcf449d74f30e2b46569","breed":"beagle","img":"https://images.dog.ceo/breeds/beagle/n02088364_9650.jpg"},
     //    {"_id":"5b4edd1749d74f30e2b4656a","breed":"akita","img":"https://images.dog.ceo/breeds/akita/Akita_Inu_dog.jpg"}]
-	  console.log('dogs',dogs)
+	  //console.log('dogs',dogs)
     return (
       <FlatList
         styles={styles.container}
@@ -103,15 +103,13 @@ const styles = StyleSheet.create({
     flex: 1
   },
   item: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
+    color: 'black',
   }
 });
 
 const mapStateToProps = state => {
 
-	console.log(state)
+	//console.log(state)
   let storedRepositories = state.dogs.map(dog => ({...dog }));
   return {
     dogs: storedRepositories
